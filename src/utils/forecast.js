7617dfe1e24ad7b0fb8bc6ff331060a2cef32611
albeertolc <albertolinarescorrales16@gmail.com>
2021-11-18 15:44:19 +0100

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f95b9965409d7ee2d3c4f81e998a9a77&query=' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+        }
+    })
+}
+
+module.exports = forecast
